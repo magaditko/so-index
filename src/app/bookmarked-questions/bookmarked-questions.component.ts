41e239c396ab7bb75bef1e4651789233f0142e98
Margarita Vasileva <m.h.vasileva@gmail.com>
2022-11-29 19:12:37 +0200

    Created placeholder components

---
+++ src/app/bookmarked-questions/bookmarked-questions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarked-questions',
+  templateUrl: './bookmarked-questions.component.html',
+  styleUrls: ['./bookmarked-questions.component.scss']
+})
+export class BookmarkedQuestionsComponent {
+
+}
