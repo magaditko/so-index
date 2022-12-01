1ab9a38f8ed610dcc8b7fc2eb21ef28b5cf0fff9
Margarita Vasileva <m.h.vasileva@gmail.com>
2022-12-01 19:57:55 +0200

    Add common question component

---
+++ src/app/question/question.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Question, SoState } from '../models';
+import { addBookmark, loadQuestion, removeBookmark } from '../store/feed.actions';
+import { faBookmark as bookmarkSolid } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as bookmarkRegular } from '@fortawesome/free-regular-svg-icons'
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+  constructor(private store: Store<SoState>, private router: Router, private route: ActivatedRoute) {}
+
+  @Input() question: Question | undefined;
+  @Input() bookmarks: number[] | any;
+  @Input() showDetails: boolean = false;
+
+  faBookmarkSolid = bookmarkSolid;
+  faBookmarkRegular = bookmarkRegular;
+
+  ngOnInit() {}
+
+  addToBookmarks(question: Question): void {
+    this.store.dispatch(addBookmark({question: question}))
+  }
+
+  removeFromBookmarks(question: Question): void {
+    this.store.dispatch(removeBookmark({question: question}))
+  }
+
+  goToDetails(questionId: number): void {
+    this.store.dispatch(loadQuestion({questionId: questionId}))
+    this.router.navigate(['/question-details'], { replaceUrl: true });
+  }
+
+  checkBookmarked(questionId: number, bookmarked: Question[]) {
+    let element = bookmarked.findIndex(el => el.question_id === questionId)
+    return element >= 0;
+  }
+}
