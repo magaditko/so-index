df371bfa31c6a994a0fc0c2472019544ba70073d
Margarita Vasileva <m.h.vasileva@gmail.com>
2022-12-01 20:04:58 +0200

    Add logic to bookmarked page

--- src/app/bookmarked-questions/bookmarked-questions.component.ts
+++ src/app/bookmarked-questions/bookmarked-questions.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { first, map, mergeMap, Observable, of, switchMap, tap } from 'rxjs';
+import { Question, SoState } from '../models';
+import { loadBookmarked, loadQuestion } from '../store/feed.actions';
+import { getBookmarked, getBookmarkedFeed } from '../store/feed.selectors';
 
 @Component({
   selector: 'app-bookmarked-questions',
   templateUrl: './bookmarked-questions.component.html',
   styleUrls: ['./bookmarked-questions.component.scss']
 })
-export class BookmarkedQuestionsComponent {
+export class BookmarkedQuestionsComponent implements OnInit{
+  bookmarks: Question[] | undefined;
+  bookmarked$: Observable<Question[]> | undefined;
 
+  constructor(private store: Store<SoState>) {}
+  
+  ngOnInit(): void {
+    this.bookmarked$ = this.store.pipe(select(getBookmarkedFeed)); 
+    this.store.pipe(select(getBookmarked), first()).subscribe(data => {
+      this.bookmarks = data;
+      let ids = data.map(q => {
+        return q.question_id
+      });
+      if(data.length > 0) {
+        this.store.dispatch(loadBookmarked({questionIds: ids}));
+      }
+    });
+  }
 }
